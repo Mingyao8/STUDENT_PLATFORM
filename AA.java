117cdc2843a7135e54d92c2604297e15ee91978e
MingYao <96293549+Mingyao8@users.noreply.github.com>
2023-03-23 18:49:49 +0800

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,11 @@
+package AB;
+
+public class AA {
+
+	public static void main(String[] args) {
+		Function fun = new Function();
+		int flag = fun.find("410977000");
+		System.out.println(flag);
+	}
+
+}
